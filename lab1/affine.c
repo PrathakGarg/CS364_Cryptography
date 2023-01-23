301166f4d81830316d2aac119c67f2d1f882c395
Phoenix <prodotzip@gmail.com>
2023-01-23 16:55:58 +0530

    Revert "Lab 2 - Playfair Cipher"
    This reverts commit fcc23c13011aac05321808d54d98295c036b0c7a.

--- lab1/affine.c
+++ lab1/affine.c
@@ -18,20 +18,6 @@ int inverse(int a) {
     }
 }
 
-void encrypt(char plaintext[], int a, int b, char ciphertext[]) {
-    for (int i=0; i<strlen(plaintext); i++) {
-        ciphertext[i] = (((tolower(plaintext[i]) - 'a') * a) + b) % 26 + 'a';
-    }
-    ciphertext[strlen(plaintext)] = '\0';
-}
-
-void decrypt(char ciphertext[], int a, int b, char decrypted[]) {
-    for (int i=0; i<strlen(ciphertext); i++) {
-        decrypted[i] = ((tolower(ciphertext[i]) - 'a' + 26 - b) * inverse(a)) % 26 + 'a';
-    }
-    decrypted[strlen(ciphertext)] = '\0';
-}
-
 void main() {
     char plaintext[100], ciphertext[100], decrypted[100];
     int a, b;
@@ -40,8 +26,12 @@ void main() {
     scanf("%d %d", &a, &b);
     assert(gcd(a, 26) == 1);
 
-    encrypt(plaintext, a, b, ciphertext);
-    decrypt(ciphertext, a, b, decrypted);
+    for (int i=0; i<strlen(plaintext); i++) {
+        ciphertext[i] = (((tolower(plaintext[i]) - 'a') * a) + b) % 26 + 'a';
+        decrypted[i] = ((tolower(ciphertext[i]) - 'a' + 26 - b) * inverse(a)) % 26 + 'a';
+    }
+    ciphertext[strlen(plaintext)] = '\0';
+    decrypted[strlen(plaintext)] = '\0';
 
     printf("%s\n%s\n", ciphertext, decrypted);
 }
--- lab1/shift.c
+++ lab1/shift.c
@@ -2,28 +2,16 @@
 #include <ctype.h>
 #include <string.h>
 
-void encrypt(char plaintext[], char shift, char ciphertext[]) {
-    for (int i = 0; i < strlen(plaintext); i++) {
-        ciphertext[i] = (tolower(plaintext[i]) - 'a' + tolower(shift) - 'a') % 26 + 'a';
-    }
-    ciphertext[strlen(plaintext)] = '\0';
-}
-
-void decrypt(char ciphertext[], char shift, char decrypted[]) {
-    for (int i = 0; i < strlen(ciphertext); i++) {
-        decrypted[i] = (ciphertext[i] - 'a' - tolower(shift) + 'a' + 26) % 26 + 'a';
-    }
-    decrypted[strlen(ciphertext)] = '\0';
-}
-
 void main() {
     char plaintext[100], ciphertext[100], decrypted[100];
     char shift;
     scanf("%s", plaintext);
     scanf("\n%c", &shift);
-    
-    encrypt(plaintext, shift, ciphertext);
-    decrypt(ciphertext, shift, decrypted);
-
+    for (int i = 0; i < strlen(plaintext); i++) {
+        ciphertext[i] = (tolower(plaintext[i]) - 'a' + tolower(shift) - 'a') % 26 + 'a';
+        decrypted[i] = (ciphertext[i] - 'a' - tolower(shift) + 'a' + 26) % 26 + 'a';
+    }
+    ciphertext[strlen(plaintext)] = '\0';
+    decrypted[strlen(plaintext)] = '\0';
     printf("Ciphertext: %s\nDecrypted: %s\n", ciphertext, decrypted);
 }
